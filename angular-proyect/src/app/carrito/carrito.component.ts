05ff43503d12bfd5dbff6a1af925629c524d369a
Lucafu49 <lucasmena1403@gmail.com>
2023-10-22 20:16:23 -0300

    Creada la estructura de Carrito, tira error

--- angular-proyect/src/app/carrito/carrito.component.ts
+++ angular-proyect/src/app/carrito/carrito.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent {
+  IDlibrosEnCarrito: number[]=[];
+  usuarioId: number;
+
 
 }
